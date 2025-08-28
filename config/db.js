90ca8ad1e97fb9ee090b7abb25746c2d9ab89e32
Aditi <132155126+aditi32145@users.noreply.github.com>
2025-08-28 12:16:19 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost", 
-  database: "mlm_crypto",
-  password: "aditi@500",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
-
 
 pool.connect()
   .then(() => console.log("✅ Connected to PostgreSQL"))
