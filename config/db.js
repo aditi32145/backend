7608cbaa1c3cba46e5d55c9ad6b71570e214824d
Aditi <132155126+aditi32145@users.noreply.github.com>
2025-08-28 12:22:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
+// config/db.js
 const { Pool } = require("pg");
 require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -7,6 +10,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 pool.connect()
